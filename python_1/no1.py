e89dfc03c93028371121e278cf24f53a67361a06
jairodiazg <jairodiazg@icloud.com>
2024-11-13 17:44:44 -0500

    python calc program

--- python_1/no1.py
+++ python_1/no1.py
@@ -1,5 +1,51 @@
-print("Welcome to the APPLE SAUCE PROGRAM!")
+def displayMenu():
+    print("1 Addition")
+    print("2 Subtraction")
+    print("3 Multiplication")
+    print("4 Division")
 
-numAppleSauce = input("How many apple sauces do you like?")
+def addNums():
+    ufirstNum = float(input("First number: "))
+    usecondNum = float(input("Second number: "))
+    total = ufirstNum + usecondNum
+    print(f"The sum of {ufirstNum} and {usecondNum} is {total}")
 
-print("The user said ", numAppleSauce)
+def subNums():
+    ufirstNum = float(input("First number: "))
+    usecondNum = float(input("Second number: "))
+    diff = ufirstNum - usecondNum
+    print(f"The difference between {ufirstNum} and {usecondNum} is {diff}")
+
+def multNums():
+    ufirstNum = float(input("First number: "))
+    usecondNum = float(input("Second number: "))
+    product = ufirstNum * usecondNum
+    print(f"The product of {ufirstNum} and {usecondNum} is {product}")
+
+def divNums():
+    ufirstNum = float(input("First number: "))
+    usecondNum = float(input("Second number: "))
+    if usecondNum == 0:
+        print("dividing by zero is not sigma")
+    else:
+        quotient = ufirstNum / usecondNum
+        print(f"The quotient of {ufirstNum} and {usecondNum} is {quotient}")
+
+def main():
+    displayMenu()
+    uOption = int(input("select a sigma mode: "))
+
+    if uOption == 1:
+        addNums()
+    elif uOption == 2:
+        subNums()
+    elif uOption == 3:
+        multNums()
+    elif uOption == 4:
+        divNums()
+    else:
+        print("Non sigma option. choose a number between 1 and 4")
+
+print("Welcome to the sigma calc (calc is slang for calculator btw)")
+
+main()
